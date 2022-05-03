2d388dc108aed17f8e0ffcdc6b8ed7f5bcc660c6
dhkim <skku.dhkim@gmail.com>
2022-05-03 23:58:59 +0900

    FedAvg method simplified and data quantity is measured now.

--- src/train/fed_aggregator.py
+++ src/train/fed_aggregator.py
@@ -5,34 +5,41 @@ import numpy as np
 import warnings
 import ray
 import torch
+import re
 
-from src.model import model_manager
+from src.model import model_manager, custom_cnn
 from collections import OrderedDict
-#from src.train.similarity import cosin_clustering, uclidean_clustering
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import confusion_matrix
 
 # warnings.filterwarnings("ignore")
 
 
 # @ray.remote(num_cpus=1)
 class Aggregator:
-    def __init__(self, test_data, model_name: str, global_lr: float, log_path: str):
-        self.global_model = model_manager.get_model(model_name)
+    def __init__(self, test_data, model_name: str, num_of_classes: int, global_lr: float, log_path: str):
+        self.global_model = model_manager.get_model(model_name, num_of_classes=num_of_classes)
         self.lr = global_lr
-        # self.total_data_len = 0
-        self.empty_model = copy.deepcopy(self.global_model.state_dict())
-        self.test_data = test_data
+        self.test_loader = test_data
         self.summary_path = os.path.join(log_path, "tensorboard/global")
         self.global_iter = 0
 
     def fedAvg(self, collected_weights):
-        empty_model = self.global_model.make_empty_weights()
+        empty_model = {}
+        total_len = 0
 
-        for k, v in empty_model.items():
-            for client in collected_weights:
-                empty_model[k] += self.lr * (client[k] / len(collected_weights))
+        for _, data_size in collected_weights:
+            total_len += data_size
 
-        self.global_model.set_weights(empty_model)
+        for k, v in self.global_model.state_dict().items():
+            for weights, data_size in collected_weights:
+                if k not in empty_model.keys():
+                    empty_model[k] = self.lr * weights[k] * (data_size / total_len)
+                else:
+                    empty_model[k] += self.lr * weights[k] * (data_size / total_len)
+
+        # Global model updates
+        self.global_model.load_state_dict(empty_model)
         self.global_iter += 1
 
     def evaluation(self, device):
@@ -45,32 +52,3 @@ class Aggregator:
         with SummaryWriter(self.summary_path) as writer:
             writer.add_scalar('global_acc', accuracy.item(), self.global_iter)
         return accuracy
-
-    # def get_weights(self, deep_copy=False):
-    #     if deep_copy:
-    #         return copy.deepcopy(self.global_model.state_dict())
-    #     return self.global_model.state_dict()
-    #
-    # def set_weights(self, weights):
-    #     self.global_model.load_state_dict(weights)
-
-    # def cosin_clustering(self, weights, original_shape):
-    #     kmeans = KMeans(n_clusters=original_shape[1])
-    #     lables = kmeans.fit_predict(weights)
-    #     # lables = kmeans.labels_
-    #     centers = kmeans.cluster_centers_
-    #     print(lables)
-    #     print(centers)
-    #     # print(centers)
-    #     # _csr_matrix = csr_matrix(weights)
-    #     # spherical_kmeans = SphericalKMeans(
-    #     #     n_clusters=original_shape[1],
-    #     #     max_iter=10,
-    #     #     verbose=1,
-    #     #     init='similar_cut',
-    #     #     sparsity='minimum_df',
-    #     #     minimum_df_factor=0.05
-    #     # )
-    #     # labels = spherical_kmeans.fit_predict(_csr_matrix)
-    #     # print(labels)
-    #     print("--"*10+"Next"+"--"*10)
