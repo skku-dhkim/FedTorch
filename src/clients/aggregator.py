67d55a9f2ad3a8fb3d112b734db6956de00a5c98
davidshyn1 <59733867+davidshyn1@users.noreply.github.com>
2022-07-29 04:01:58 +0900

    Additionally Update aggregator.py
    Learning Rate Adaptation Algorithm is held in each client.
    
    1. Remove all codes related to Learning Rate Adaptation Algorithm
    (1) delete fixed_lr function
    (2) delete sim_list  function
    (3) delete get_lrs function
    (4) delete get_sim function
    (5) modify fedavg function(weight aggregation part)
    
    2. the input 'num_of_clients' is not needed --> delete 'num_of_clients' + remove modified parts of __init__ function

--- src/clients/aggregator.py
+++ src/clients/aggregator.py
@@ -12,7 +12,6 @@ class Aggregator:
                  dataset_name: str,
                  log_path: str,
                  train_settings: dict,
-                 num_of_clients: int,
                  **kwargs):
         # Client Meta setting
         self.name = 'aggregator'
@@ -49,9 +48,6 @@ class Aggregator:
         self.original_rep = self.compute_representations()
         self.cos_sim = torch.nn.CosineSimilarity(dim=-1).to(self.device)
 
-        self.num_of_clients = num_of_clients
-        self.lrs = [0.1 for i in range(num_of_clients)]
-        self.sim_l = [1.0 for i in range(num_of_clients)]
     @property
     def collected_weights(self):
         return self.__collected_weights
@@ -68,11 +64,6 @@ class Aggregator:
             return OrderedDict({k: v.clone().detach().cpu() for k, v in self.model.state_dict().items()})
         else:
             return [val.clone().detach().cpu() for _, val in self.model.state_dict().items()]
-    
-    def get_lrs(self):
-        return self.lrs
-    def get_sim(self):
-        return self.sim_l
 
     def save_model(self):
         save_path = os.path.join(self.summary_path, "model")
@@ -159,9 +150,9 @@ class Aggregator:
         for k, v in self.model.state_dict().items():
             for _, client in self.collected_weights.items():
                 if k not in empty_model.keys():
-                    empty_model[k] = original_model[k] + (client['weights'][k] - original_model[k]) *self.lr*self.lrs[int(_)]
+                    empty_model[k] = client['weights'][k] * (client['data_len']/total_len) * self.lr
                 else:
-                    empty_model[k] += (client['weights'][k] - original_model[k]) *self.lr*self.lrs[int(_)]
+                    empty_model[k] += client['weights'][k] * (client['data_len']/total_len) * self.lr
 
         # Global model updates
         self.set_parameters(empty_model)
@@ -179,55 +170,6 @@ class Aggregator:
 
         if model_save:
             self.save_model()
-
-    ##proposed method
-    ## method for learning rate adaptation based on fixed cosine value
-    def fixed_lr(self, original_state: OrderedDict,target_sim = 0.99):#99999
-        original_params = [] ##glob_w
-        local_params = [[] for _, client in self.collected_weights.items()] ##
-        grad_params = [[] for _, client in self.collected_weights.items()] ##calc_grad
-        
-        lr_list = [[] for _, client in self.collected_weights.items()]
-        
-        for k, v in self.model.state_dict().items():
-            original_params.append(torch.flatten(original_state[k].to(torch.float32)))
-            for _, client in self.collected_weights.items():
-                local_params[int(_)].append(torch.flatten(client['weights'][k].to(torch.float32)))
-            
-        original_params = torch.cat(original_params) ## flatten and change to vector 
-        
-        for _,client in self.collected_weights.items():
-            local_params[int(_)]= torch.cat(local_params[int(_)]) ## flatten and change to vector
-            grad_params[int(_)] = local_params[int(_)] - original_params
-        
-        ## calculate learning rate on each clients
-        summ = 0.0
-        
-        for _,client in self.collected_weights.items():
-            A = torch.dot(grad_params[int(_)],original_params)
-            G = torch.dot(original_params,original_params)
-            dG = torch.dot(grad_params[int(_)], grad_params[int(_)])
-            T = target_sim**2
-            C = self.cos_sim(grad_params[int(_)],original_params)**2
-            
-            dtr = ((1.0-T)*A)**2 + G*dG*(1.0-T)*T*(T-C)
-            sq = torch.sqrt(dtr)
-            head = (1.0-T)*A +sq
-            body = (T-C) * dG ##warning!! this part coud be "too small" to be denominator
-            
-            
-            
-            X = head/body
-            if torch.isnan(X) or torch.isinf(X):
-                X = 10.0
-
-            #result = head/body ##this part should be clipped
-            result = torch.clamp(X,-10.0,10.0)
-            lr_list[int(_)] = result
-
-        
-        return lr_list 
-
 
     def calc_cos_similarity(self, original_state: OrderedDict, current_state: OrderedDict) -> Optional[OrderedDict]:
         result = OrderedDict()
@@ -247,25 +189,3 @@ class Aggregator:
         self.summary_writer.add_scalar("rep_similarity_after", score, self.global_iter)
         self.original_rep = current_rep
         return score
-
-    def sim_list(self, original_state: OrderedDict,target_sim = 1 - (1e-15)):#99999
-        original_params = [] ##glob_w
-        local_params = [[] for _, client in self.collected_weights.items()] ##
-        
-        sim_list = [[] for _, client in self.collected_weights.items()]
-        
-        for k, v in self.model.state_dict().items():
-            original_params.append(torch.flatten(original_state[k].to(torch.float32)))
-            for _, client in self.collected_weights.items():
-                local_params[int(_)].append(torch.flatten(client['weights'][k].to(torch.float32)))
-            
-        original_params = torch.cat(original_params) ## flatten and change to vector 
-        
-        for _,client in self.collected_weights.items():
-            local_params[int(_)]= torch.cat(local_params[int(_)]) ## flatten and change to vector
-               
-        for _,client in self.collected_weights.items():
-            C = self.cos_sim(local_params[int(_)],original_params)
-            sim_list[int(_)] = C
-        
-        return sim_list
