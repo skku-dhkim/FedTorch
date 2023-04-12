43f38b32564c16b694aa9a1c72690d7cf79f054f
dhkim <skku.dhkim@gmail.com>
2023-04-12 13:46:30 +0900

    Issue #16 fixed.

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -3,6 +3,7 @@ import copy
 import torch.nn.functional as F
 
 from .custom_cnn import CustomCNN, ModelFedCon
+from .resnet import ResNet50_cifar10
 from torchvision.models import *
 from torch.nn import Sequential, Linear, ReLU
 
@@ -13,13 +14,7 @@ def model_call(model_name: str, num_of_classes: int):
     if model_name.lower() == 'moon_cnn':
         return ModelFedCon(10, n_classes=num_of_classes)
     if model_name.lower() == "resnet-50":
-        _model = resnet50()
-        fc = Sequential(
-            Linear(in_features=2048, out_features=1000, bias=True),
-            ReLU(inplace=True),
-            Linear(in_features=1000, out_features=num_of_classes, bias=True)
-        )
-        _model.fc = fc
+        _model = ResNet50_cifar10(num_classes=num_of_classes)
         return _model
     if model_name.lower() == "resnet-18":
         _model = resnet18()
